9761779e9b9351e59bfe469a8edaf17e807f0882
Manuel Garaboa <mannygaraboa@gmail.com>
2024-07-23 23:09:21 -0400

    Kata Complete

--- 8kyu/July-2024/Super-Duper-Easy/index.js
+++ 8kyu/July-2024/Super-Duper-Easy/index.js
@@ -1,14 +1,19 @@
 // Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
 
 function problem(x){
-    let total = [(x*50) + 6];
-    if(total == NaN)
+    if(isNaN(x) || x === "")
     {
-        console.log("This is a string");
-        return Error;
+        console.log("Error")
+        return "Error";
     }
-    console.log("Here is my total: " + total);
-
-    return 0;
+    else
+    {
+        let total = (x*50) + 6;
+        console.log("Here is my total: " + total);
+        return (x*50)+6;
+    }
 }
-problem("4");
+problem(0);
+
+// Best Practice:
+// const problem = x => typeof x === 'string' ? 'Error' : x * 50 + 6;
