41da98e42d897ff4d86a80d3ee5b73eef4fd3e13
Manuel Garaboa <mannygaraboa@gmail.com>
2024-07-25 23:42:15 -0400

    Python Testing

--- 8kyu/July-2024/Super-Duper-Easy/index.py
+++ 8kyu/July-2024/Super-Duper-Easy/index.py
@@ -1,6 +1,11 @@
 # Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
 
 def problem(a):
-    return (a*50) + 6
+    if a == str:
+        print("Error")
+        return 0
+    else:
+        print((a*50) + 6)
+        return (a*50) + 6
 
-problem(4)
+problem(90)
