c9fca0b880a9e846013e9286ab516630dcb5afb4
Manuel Garaboa <mannygaraboa@gmail.com>
2024-08-06 23:52:04 -0400

    Super-Duper-Easy Kata: Python Solution

--- 8kyu/July-2024/Super-Duper-Easy/index.py
+++ 8kyu/July-2024/Super-Duper-Easy/index.py
@@ -1,11 +1,19 @@
 # Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
 
 def problem(a):
-    if a == str:
-        print("Error")
-        return 0
+    if isinstance(a, str):
+        print(a, ": Error")
+        return "Error"
     else:
-        print((a*50) + 6)
-        return (a*50) + 6
+        total = (a*50) + 6;
+        print("Total:", total)
+        return total
 
-problem(90)
+problem("Hello")
+
+# Best Practice:
+# def problem(a):
+#     try:
+#         return a * 50 + 6
+#     except TypeError:
+#         return "Error"
