606db19e00e392bac262905c9177bad9857ef495
Manuel Garaboa <mannygaraboa@gmail.com>
2024-08-31 21:26:58 -0400

    Likes Vs Dislikes: Start

---
+++ 7kyu/August-2024/Likes-Vs-Dislikes/script.js
@@ -0,0 +1,7 @@
+// Like, Dislike, Nothing come from Preloaded
+
+function likeOrDislike(buttons) {
+    // return Like || Dislike || Nothing ;
+    console.log(buttons)
+}
+likeOrDislike("Like");
---
+++ 7kyu/August-2024/Likes-Vs-Dislikes/script.py
@@ -0,0 +1,7 @@
+# from preloaded import Like, Dislike, Nothing
+
+def like_or_dislike(lst):
+    # return Like or Dislike or Nothing
+    print(lst)
+
+like_or_dislike([1, 2, 3])
