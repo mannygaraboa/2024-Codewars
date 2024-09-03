44e523a3a5dd892e2ae34da7b462dda4e5e2ad04
Manuel Garaboa <mannygaraboa@gmail.com>
2024-09-02 22:38:08 -0400

    Likes Vs Dislikes: Python Solution

--- 7kyu/August-2024/Likes-Vs-Dislikes/script.py
+++ 7kyu/August-2024/Likes-Vs-Dislikes/script.py
@@ -1,7 +1,41 @@
 # from preloaded import Like, Dislike, Nothing
 
 def like_or_dislike(lst):
-    # return Like or Dislike or Nothing
-    print(lst)
+    status = 0
 
-like_or_dislike([1, 2, 3])
+    if(lst == []):
+        return "Nothing"
+    else:
+        for i in lst:
+            if(i == "Like"):
+                if(status == 0 or status == -1):
+                    if(status == 0):
+                        status = status + 1
+                    elif(status == -1):
+                        status = status + 2
+                elif(status == 1):
+                    status = status - 1
+            elif(i == "Dislike"):
+                if(status == 0 or status == 1):
+                    if(status == 0):
+                        status = status - 1
+                    elif(status == 1):
+                        status = status - 2
+                elif(status == -1):
+                    status = 0
+
+    if(status == 0):
+        return "Nothing"
+    elif(status == 1):
+        return "Like"
+    elif(status == -1):
+        return "Dislike"               
+
+like_or_dislike(["Like", "Like", "Dislike"])
+
+# Best Practice:
+    # def like_or_dislike(lst):
+    # 	state = 'Nothing'
+    # 	for i in lst:
+    # 		state = 'Nothing' if i == state else i
+    # 	return state
