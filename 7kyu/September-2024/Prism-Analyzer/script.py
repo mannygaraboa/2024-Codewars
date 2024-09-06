3fc4c1ac48755c026f8ca5b8e565abacd472d0fe
Manuel Garaboa <mannygaraboa@gmail.com>
2024-09-06 18:16:22 -0400

    Prism Analyzer: Kata Start

---
+++ 7kyu/September-2024/Prism-Analyzer/script.py
@@ -0,0 +1,19 @@
+# In this kata, you are given the sum of the number of vertices, edges, and faces of an
+# N-sided prism, which is equal to s. Your task is to determine and return a tuple containing four values: the number of vertices, edges,
+# faces, and the number of sides of the base of the prism.
+# A prism is a polyhedron with two parallel, congruent bases and rectangular lateral faces connecting the corresponding sides of the bases.
+# triangular prism example:
+# For an N-sided prism:
+# Vertices (V): The total number of vertices.
+# Edges (E): The total number of edges.
+# Faces (F): The total number of faces.
+# Base sides (B): The number of sides of the base of the prism.
+# Given the sum s = V + E + F return the tuple (V ,E ,F ,B).
+
+# Constraints:
+#     1. The number of sides of the base B is an integer greater than or equal to 3.
+
+#     2. The sum s is an integer in the range 4 ≤ 𝑠 ≤ 1000
+
+def prism_properties(s):
+    pass
